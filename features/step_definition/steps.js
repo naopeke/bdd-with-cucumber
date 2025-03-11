499f687ad8a88bcc3ae88d6a6da036239f365e6a
naopeke <naopeke@protonmail.com>
2025-03-11 13:28:29 +0100

    shouty.js

--- features/step_definition/steps.js
+++ features/step_definition/steps.js
@@ -1,14 +1,20 @@
+const Person = require('../../src/shouty.js');
 const { Given, When, Then } = require('@cucumber/cucumber');
+const { assertThat, is } = require('hamjest');
 
 Given('Lucy is located {int} metres from Sean', function (distance){
+    this.lucy = new Person;
+    this.sean = new Person;
+    this.lucy.moveTo(distance);
     console.log(distance);
-    return 'pending'
+    // return 'pending'
 })
 
-When('Sean shouts {string}', function (string){
-    return 'pending'
+When('Sean shouts {string}', function (message){
+    this.sean.shout(message);
+    this.message = message;
 })
 
 Then('Lucy hears Sean\'s message', function(){
-    return 'pending'
+    assertThat(this.lucy.messagesHeard(), is([this.message]));
 })
